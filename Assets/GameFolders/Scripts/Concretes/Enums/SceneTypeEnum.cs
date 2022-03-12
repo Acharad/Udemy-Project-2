c8247432db2065d68c54992ea21daec47c28d650
Acharad <iahmetkavras@gmail.com>
2022-03-12 20:48:37 +0300

    GameManager - SceneTypeEnum script

---
+++ Assets/GameFolders/Scripts/Concretes/Enums/SceneTypeEnum.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UdemyProje3.Enums
+{
+    public enum SceneTypeEnum
+    {
+        Menu, Game, SplashScreen
+    }
+}
--- Assets/GameFolders/Scripts/Concretes/Managers/GameManager.cs
+++ Assets/GameFolders/Scripts/Concretes/Managers/GameManager.cs
@@ -1,63 +1,97 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
+using UdemyProje3.Enums;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+namespace UdemyProje3.Managers
 {
-    [SerializeField] private int score;
-    
-    public GameManager Instance { get; private set; }
-
-    private void Awake() 
+    public class GameManager : MonoBehaviour
     {
-        SingeltonThisObject();
-    }
+        [SerializeField] int score;
 
-    private void SingeltonThisObject()
-    {
-        if (Instance == null)
+        private const string PLAYER_SCORE = "Player_Score";
+
+        public static GameManager Instance { get; private set; }
+
+        public int Score => score;
+
+        public event System.Action<SceneTypeEnum> OnSceneChanged;
+        public event System.Action<int> OnScoreChanged;
+
+        private void Awake()
         {
-            Instance = this;
-            DontDestroyOnLoad(this.gameObject);
+            SingletonThisObject();
         }
-        else 
-            Destroy(this.gameObject);
-    }
 
-    public void SplashScreen()
-    {
-        StartCoroutine(SplashScreenAsync());
-    }
-    
-    private IEnumerator SplashScreenAsync()
-    {
-        yield return SceneManager.LoadSceneAsync("SplashScreen");
+        private IEnumerator Start()
+        {
+            yield return SceneManager.LoadSceneAsync("Menu", LoadSceneMode.Additive);
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName("Menu"));
 
-        yield return new WaitForSeconds(3f);
-        
-        StartGame();
-    }
-    
-    public void StartGame()
-    {
-        StartCoroutine(StartGameAsync());
-    }
+            if (PlayerPrefs.HasKey(PLAYER_SCORE))
+            {
+                score = PlayerPrefs.GetInt(PLAYER_SCORE);
+            }
+        }
 
-    private IEnumerator StartGameAsync()
-    {
-        yield return SceneManager.LoadSceneAsync("Game");
-    }
+        private void SingletonThisObject()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
+        }
 
-    public void ReturnMenu()
-    {
-        
-    }
+        public void SplashScreen(SceneTypeEnum sceneTypeEnum)
+        {
+            StartCoroutine(SplashScreenAsync(sceneTypeEnum));
+        }
 
-    public void QuitGame()
-    {
-        Application.Quit();
+        private IEnumerator SplashScreenAsync(SceneTypeEnum sceneType)
+        {
+            yield return SceneManager.LoadSceneAsync(SceneTypeEnum.SplashScreen.ToString(), LoadSceneMode.Additive);
+            OnSceneChanged?.Invoke(SceneTypeEnum.SplashScreen);
+
+            yield return SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName("SplashScreen"));
+
+            yield return new WaitForSeconds(5f);
+
+            yield return SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+            yield return SceneManager.LoadSceneAsync(sceneType.ToString(), LoadSceneMode.Additive);
+
+            OnSceneChanged.Invoke(sceneType);
+
+            SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneType.ToString()));
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+
+        public void IncreaseScore(int scorePoint)
+        {
+            score += scorePoint;
+            OnScoreChanged?.Invoke(score);
+        }
+
+        public void DecreaseScore(int scorePoint)
+        {
+            score -= scorePoint;
+            OnScoreChanged?.Invoke(score);
+        }
+
+        public void SaveScore()
+        {
+            PlayerPrefs.SetInt(PLAYER_SCORE, score);
+        }
     }
 }
-
